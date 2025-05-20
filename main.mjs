ccd91ad55dea4d8689da375fc488923bc7e5064e
tomoharukawamura <kmura.5146d202@gmail.com>
2025-05-20 13:11:58 +0900

    エラーハンドリング追加

--- main.mjs
+++ main.mjs
@@ -1,6 +1,7 @@
 import express from 'express'
 import { CarManager } from './lib/car-manager.mjs'
 import { Worker } from 'worker_threads'
+import { errorNotifyClient } from './lib/messaging-api-client.mjs'
 
 const app = express()
 const port = Number(process.env.PORT) || 3000
@@ -8,31 +9,42 @@ const carManager = new CarManager()
 
 app.listen(port, async () => {
     console.log(`Server is running on port ${port}`);
-
-    await carManager.getCars({ resetNewCars: false, resetSoldCars: false })
-    while (true) {
-      await carManager.getCars({ resetNewCars: true, resetSoldCars: true })
-
-      if (!carManager.newCars.length && !carManager.soldOut.length) {
-        setTimeout(() => {
-          Promise.resolve()
-        }, 1000 * 30)
-      } else {
-        if (carManager.newCars.length) {
-          const classifiedNewCars = carManager.classifyCars(carManager.newCars)
-          const newCarWorkers = classifiedNewCars.map(cnc => {
-            return new Worker('./lib/worker.mjs', { workerData: { ...cnc, type: 'new' }})
-          })
-          carManager.newCars = []
-        }
-        if (carManager.soldOut.length) {
-          const classifiedSoldOutCars = carManager.classifyCars(carManager.soldOut)
-          const soldOutCarWorkers = classifiedSoldOutCars.map(csoc => {
-            return new Worker('./lib/worker.mjs', { workerData: { ...csoc, type: 'soldOut' }})
-          })
-          carManager.soldOut = []
+    try {
+      await carManager.getCars({ resetNewCars: false, resetSoldCars: false })
+      while (true) {
+        await carManager.getCars({ resetNewCars: true, resetSoldCars: true })
+  
+        if (!carManager.newCars.length && !carManager.soldOut.length) {
+          setTimeout(() => {
+            Promise.resolve()
+          }, 1000 * 30)
+        } else {
+          if (carManager.newCars.length) {
+            const classifiedNewCars = carManager.classifyCars(carManager.newCars)
+            const newCarWorkers = classifiedNewCars.map(cnc => 
+              new Worker('./lib/worker.mjs', { workerData: { ...cnc, type: 'new' }})
+              .on('error', error => { throw error })
+            )
+            carManager.newCars = []
+          }
+          if (carManager.soldOut.length) {
+            const classifiedSoldOutCars = carManager.classifyCars(carManager.soldOut)
+            const soldOutCarWorkers = classifiedSoldOutCars.map(csoc => 
+              new Worker('./lib/worker.mjs', { workerData: { ...csoc, type: 'soldOut' }})
+              .on('error', error => { throw error })
+            )
+            carManager.soldOut = []
+          }
         }
       }
+    } catch (error) {
+      await errorNotifyClient.pushMessage({
+        to: process.env.LINE_USER_ID,
+        messages: [{
+          type: 'text',
+          text: JSON.stringify(error.body)
+        }]
+      })
     }
   }
 );

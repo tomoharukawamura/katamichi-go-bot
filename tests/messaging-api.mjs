53092a62e541824c5519503cd21191e741075b82
tomoharukawamura <kmura.5146d202@gmail.com>
2025-05-26 00:59:33 +0900

    completed

--- tests/messaging-api.mjs
+++ tests/messaging-api.mjs
@@ -1,5 +1,6 @@
 import { Worker } from 'worker_threads';
 import { CarManager } from '../lib/car-manager.mjs';
+import { redisClient } from '../lib/redis-client.mjs';
 
 const areadata = [
   ['2','3'], 
@@ -13,22 +14,56 @@ const areadata = [
 ]
 
 const carData = {
-  carName: 'グランドキャビン　車両番号865',
+  carName: 'アルファード 車両番号865',
   startShop: 'トヨタレンタリース福島 いわき平店',
-  returnShop: 'トヨタレンタリース大阪',
+  returnShop: 'トヨタモビリティサービス',
   condition: '禁煙・8人乗',
   date: '2025/5/22 ~ 2025/5/25', 
   phone: '0746321020', 
   startArea: '2', 
   returnArea: '3' ,
-  type: 'new'
+  type: 'updated',
 }
 
-const type = process.env.TYPE_FOR_TEST
-areadata.forEach(([startArea, returnArea]) => {
-  console.log('push-message')
-  new Worker('./lib/worker.mjs', { workerData: { startArea, returnArea, cars: [...Array.from({ length: 1 })].map(_i => carData), type } })
+const car2 = {
+  carName: 'ヤリス 車両番号865',
+  startShop: 'トヨタレンタリース福島 いわき平店',
+  returnShop: 'トヨタモビリティサービス',
+  condition: '禁煙・5人乗',
+  date: '2025/5/22 ~ 2025/5/25', 
+  phone: '0746321020', 
+  startArea: '2', 
+  returnArea: '3' ,
+  type: 'recovered',
+}
+
+const type = process.env.TYPE_FOR_TEST;
+const tsData = await redisClient.hgetall('car_ts_data')
+Promise.all([carData, carData, carData, carData, carData].map(car => 
+  new Promise((resolve) => {
+    new Worker('./lib/worker.mjs', { workerData: { car, ts: tsData[car.carName] || null } })
+    .on('error', error => { throw error })
+    .on('message', async (data) => {
+      tsData[car.carName] = data.ts;
+    })
+    .on('exit', (code) => {
+      if (code == 0) {
+        resolve()
+      }
+    });
+  })
+)).then(async () => {
+  await redisClient.hset('car_ts_data', tsData);
+  await redisClient.quit();
 })
+// carManager.newCars = []
+// if (carManager.soldOut.length) {
+//   await slackApp.client.chat.postMessage({
+//     channel: process.env.SLACK_CHANNEL_ID_SOLD,
+//     attachments: carManager.soldOut.map(createAttachments)
+//   })
+//   carManager.soldOut = []
+// }
 
 // const cm = new CarManager()
 // await cm.getCars({ resetNewCars: false, resetSoldCars: false })

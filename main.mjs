a047d6e545b48753777770195e7e56d215c3f9d4
tomoharukawamura <kmura.5146d202@gmail.com>
2025-05-26 10:08:24 +0900

    fix

--- main.mjs
+++ main.mjs
@@ -8,33 +8,38 @@ import { redisClient } from './lib/redis-client.mjs'
 
 const carManager = new CarManager()
 
+const notifyNewCars = async () => {
+  const tsData = await redisClient.hgetall('car_ts_data')
+  Promise.all(carManager.newCars.map(car => 
+    new Promise((resolve) => {
+      const ts = tsData[car.carName] ?? null
+      new Worker('./lib/worker.mjs', { workerData: { car , ts } })
+      .on('error', error => { throw error })
+      .on('message', (data) => {
+        tsData[car.carName] = data.ts
+      })
+      .on('exit', (code) => {
+        if (code == 0) {
+          resolve()
+        }
+      });
+    })
+  ))
+  .then(async () => {
+    await redisClient.hset('car_ts_data', tsData)
+  })
+  .finally(async () => {
+    carManager.newCars = []
+    await redisClient.quit()
+  })
+}
+
 const main = async () => {
   try {
     await carManager.getCars({ isInit: false })
     const tsData = await redisClient.hgetall('car_ts_data')
     if (carManager.newCars.length) {
-      Promise.all(carManager.newCars.map(car => 
-        new Promise((resolve) => {
-          const ts = tsData[car.carName] ?? null
-          new Worker('./lib/worker.mjs', { workerData: { car , ts } })
-          .on('error', error => { throw error })
-          .on('message', (data) => {
-            tsData[car.carName] = data.ts
-          })
-          .on('exit', (code) => {
-            if (code == 0) {
-              resolve()
-            }
-          });
-        })
-      ))
-      .then(async () => {
-        await redisClient.hset('car_ts_data', tsData)
-      })
-      .finally(async () => {
-        carManager.newCars = []
-        await redisClient.quit()
-      })
+      await notifyNewCars()
     }
     if (carManager.soldOut.length) {
       await slackApp.client.chat.postMessage({
